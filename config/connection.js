265851a6ddf9626f47662e48c2e6adc6a19bdd90
Brenda Goodwin <brendanced@gmail.com>
2020-12-16 13:54:06 -0700

    working on routes

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const { truncate } = require("../models/Tag");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "mysql",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,23 +1,64 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
+const { json } = require('sequelize/types');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+  const categoryData =await Category.findAll({ include: Product}) 
+  
+  res.status(200).json(categoryData)
+  console.log(categoryData)
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const categoryData =await Category.findByPK(req.params.id, {
+      include: Product
+    });
+    if (!categoryData) {
+      res.status(404).json({message: 'sorry,no category with this id'});
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const categoryData = await Category.create(req.body);
+  res.status(200).json(categoryData);
+return res.jason(categoryData);
   // create a new category
 });
 
+
 router.put('/:id', (req, res) => {
+try{
+  const categoryData= await Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    }
+  });
+  if(!categoryData[0]) {
+    res.staus(404).json({ message: 'no category with this id'});
+    return;
+  }
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err)
+}
+  })
   // update a category by its `id` value
 });
 
