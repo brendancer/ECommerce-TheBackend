aba8878d1976ff3e096ed2056fe9d3ca555869c9
Brenda Goodwin <brendanced@gmail.com>
2020-12-18 10:04:47 -0700

    all routes functionin

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -51,7 +51,7 @@ router.post("/", (req, res) => {
   */
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      // if there are product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
